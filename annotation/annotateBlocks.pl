89d7699e012b03096f6fe8d3b0d212ba5c0944b8
MartineDeVos <mgdevos@gmail.com>
2015-10-20 16:17:28 +0200

    rearranged order of entire script, added comments

--- annotation/annotateBlocks.pl
+++ annotation/annotateBlocks.pl
@@ -1,5 +1,5 @@
 :- use_module(library(dcg/basics)).
-:- ensure_loaded("../plsheet/test").
+:- ensure_loaded(initialBlocks).
 :- ensure_loaded(annotateTerms).
 
 
@@ -8,23 +8,19 @@
 
 
 % Start from initial classification: body and context blocks
-% The context blocks are further classifid into 1) unit block,
+% The context blocks are further classified into 1) unit block,
 % 2) a. combined Quantity-Unit block, b. Quantity block,
 % 3) Phenomenon block.
+
 
 
 		 /*******************************
 		 *  1. ANNOTATE UNIT BLOCKS	*
 
 		 *******************************/
-
-% Recognize unit blocks in context blocks.
-% Context blocks may consist entirely of units, or partly.
-% If partly, the unit part is separately annotated and substracted from
-% the original context block
-remove_unit_slices :-
-	forall(block(Block,_,_), remove_unit_slices(Block)).
-
+% Recognize unit blocks in context blocks. Context blocks may consist
+% entirely of units, or partly. If partly, the unit part is separately
+% annotated and substracted from the original context block
 remove_unit_slices(Block) :-
 	block(Block,Type,DS),
 	largest_unit_slice(Block, Slice), !,
@@ -49,44 +45,10 @@ largest_unit_slice(Block, Slice) :-
 	keysort(Unique, BySize),
 	last(BySize, Size-Slice).
 
-om_label(Label,Symbol,OMUnit):-
-	atom_codes(Label,Codes),
-	phrase(unit_label(Symbol,OMUnit),Codes,[]).
-
-unit_cell(Block,X,Y,Symbol,OMUnit):-
-	block(Block,_,DS),
-	ds_inside(DS, X, Y),
-	ds_sheet(DS, Sheet),
-	cell_value(Sheet,X,Y,Label),
-	om_label(Label,Symbol,OMUnit),
-	atom_codes(Label,Codes),
-	length(Codes,Length),
-	Length > 25.
-
-text_cell(Block,X,Y,Label,DomainConcept):-
-	block(Block,_,DS),
-	ds_inside(DS, X, Y),
-	ds_sheet(DS, Sheet),
-	cell_value(Sheet,X,Y,Label),
-	label_domain_concept(Label,_, DomainConcept).
-
-% Check whether suggested slice contains more mappings to OM than
-% domain vocabulary
-ds_map_check(Block,cell_range(Sheet, SX,SY, EX,EY)):-
-	aggregate_all(count,X-Y,
-		      (ds_inside(cell_range(Sheet, SX,SY, EX,EY),X,Y),
-		       unit_cell(Block,X,Y,_,_)),
-		      UnitMap),
-	aggregate_all(count,X-Y,
-		      (ds_inside(cell_range(Sheet, SX,SY, EX,EY),X,Y),
-		       text_cell(Block,X,Y,_,_)),
-		      TextMap),
-	UnitMap > TextMap,
-	UnitMap > 1.
-
 % Find unit slices within a block, starting from unit cell candidates
-% A slice is a unit slice when it has  more mappings to OM than AGROVOC
-% A unit slice is either horizontally or vertically oriented
+% A slice is a unit slice when it has more mappings to OM than to the
+% domain ontology. A unit slice is either horizontally or vertically
+% oriented (assumption: unit slice is single row or single column)
 unit_cell_slice(Block,X,Y,ColSlice):-
 	unit_cell(Block,X,Y,_,_),
 	cell_property(Sheet,X,Y, block(Block)),
@@ -103,26 +65,59 @@ unit_cell_slice(Block,X,Y,RowSlice):-
 	ds_map_check(Block,cell_range(Sheet, SX,Y, EX,Y)),
 	RowSlice = cell_range(Sheet, SX,Y, EX,Y).
 
+% Check whether suggested slice contains more mappings to OM than
+% to the domain ontology.
+ds_map_check(Block,cell_range(Sheet, SX,SY, EX,EY)):-
+	aggregate_all(count,X-Y,
+		      (ds_inside(cell_range(Sheet, SX,SY, EX,EY),X,Y),
+		       unit_cell(Block,X,Y,_,_)),
+		      UnitMap),
+	aggregate_all(count,X-Y,
+		      (ds_inside(cell_range(Sheet, SX,SY, EX,EY),X,Y),
+		       text_cell(Block,X,Y,_,_)),
+		      TextMap),
+	UnitMap > TextMap,
+	UnitMap > 1.
+
+% If quantity-unit grammar applies, than the term is not a unit,
+% but a quantity unit
+unit_cell(Block,X,Y,Symbol,OMUnit):-
+	block(Block,_,DS),
+	ds_inside(DS, X, Y),
+	ds_sheet(DS, Sheet),
+	cell_value(Sheet,X,Y,Label),
+	om_label(Label,Symbol,OMUnit),
+	\+ quantity_unit_label(Label,Symbol,OMUnit).
+
+text_cell(Block,X,Y,Label,Concept):-
+	block(Block,_,DS),
+	ds_inside(DS, X, Y),
+	ds_sheet(DS, Sheet),
+	cell_value(Sheet,X,Y,Label),
+	label_domain_concept(Label,_, Concept).
+
 
 		 /*******************************
 		 *  2. ANNOTATE QUANTITY BLOCKS	*
 
 		 *******************************/
+% Start from context blocks
+% IF aligned with unit block THEN quantity block (assumption)
+% ELIF # quantity cells in block (of q-u of q match) >
+% compare with domain match or quant threshold
 
 
-
-quantity_unit_label(Label,Symbol,OMUnit):-
-	atom_codes(Label,Codes),
-	phrase(quantity_unit_label(Symbol,OMUnit),Codes,[]).
 
 
 
 		 /*******************************
-		 *       OM UNIT MATCHING       *
+		 *         OM MATCHING         *
 		 *******************************/
 
 omVoc('http://www.wurvoc.org/vocabularies/om-1.8/').
 
+% A unit symbol may match with either the preferred symbol of an OMUnit,
+% the alternative symbol or the description.
 get_om_symbol(Symbol,OMUnit):-
 	\+ atom_number(Symbol,_),
 	(  unit_symbol_match(Symbol,OMUnit)
@@ -143,16 +138,32 @@ unit_description_match(Symbol,OMUnit):-
         rdf(OMUnit,rdfs:label,literal(lang(en,Symbol)),OM),
 	rdf(_,om:unit_of_measure,OMUnit,OM).
 
+
+
 		 /*******************************
 		 *	    UNIT GRAMMAR	*
 		 *******************************/
 
+quantity_unit_label(Label,Symbol,OMUnit):-
+	atom_codes(Label,Codes),
+	phrase(quantity_unit_label(Symbol,OMUnit),Codes,[]).
+
+om_label(Label,Symbol,OMUnit):-
+	atom_codes(Label,Codes),
+	phrase(unit_label(Symbol,OMUnit),Codes,[]).
+
+
+% Combined quantity-unit labels always (assumption) contain a unit
+% symbol, i.e., a unit symbol that can be matched with an OM concept,
+% between brackets. Before the brackets is at least one term.
 quantity_unit_label(Symbol,OMUnit) -->
 	pre_unit,bracket_open(T),unit_label(Symbol,OMUnit),bracket_close(T).
 
 pre_unit --> pre_unit_codes(Codes),
 	{atom_codes(Term,Codes),
-	 atom(Term)}.
+	 atom(Term),
+	 length(Codes,Length),
+	 Length > 0}.
 
 pre_unit_codes([H|T]) --> [H],pre_unit_codes(T).
 pre_unit_codes([]) --> [].
@@ -163,7 +174,8 @@ bracket_open(s) --> "[".
 bracket_close(r) --> ")".
 bracket_close(s) --> "]".
 
-
+% Unit labels always contains a unit with a symbol that can be matched
+% with an OM concept. Before the unit only one term is allowed.
 unit_label(Symbol,OMUnit) -->
 	pre_term,unit(Symbol,OMUnit),post_term.
 
